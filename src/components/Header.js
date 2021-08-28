1033bcb93180c7d97ae87442bf268f4200984047
Ali <asadri721@gmail.com>
2021-08-28 17:35:59 +0430

    Header was added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,33 @@ export default function Header() {
   return (
     <Nav>
       <Logo src='./images/logo.svg' />
-      <NavMenu></NavMenu>
+      <NavMenu>
+        <a>
+          <img src='/images/home-icon.svg' alt='' />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src='/images/search-icon.svg' alt='' />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src='/images/watchlist-icon.svg' alt='' />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src='/images/original-icon.svg' alt='' />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src='/images/movie-icon.svg' alt='' />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src='/images/series-icon.svg' alt='' />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src='/images/user.png' />
     </Nav>
   );
 }
@@ -12,10 +38,58 @@ export default function Header() {
 const Nav = styled.nav`
   height: 70px;
   background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
 `;
 
 const Logo = styled.img`
   width: 80px;
 `;
 
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  padding-left: 25px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    text-decoration: none;
+    cursor: pointer;
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &:after {
+        content: '';
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        transform: scaleX(0);
+      }
+    }
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+`;
