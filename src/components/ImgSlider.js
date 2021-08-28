80755d32828e7ac17367449529d973603d5bee9c
Ali <asadri721@gmail.com>
2021-08-28 21:04:49 +0430

    main part, some of slider

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,45 @@
+import Slider from 'react-slick';
+import styled from 'styled-components';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export default function ImgSlider() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <img src='/images/slider-badging.jpg' alt='' />
+      </Wrap>
+      <Wrap>
+        <img src='/images/slider-badag.jpg' alt='' />
+      </Wrap>
+    </Carousel>
+  );
+}
+
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  .slick-list {
+    overflow: visible;
+  }
+`;
+
+const Wrap = styled.div`
+  img {
+    border: 4px solid transparent;
+    border-radius: 4px;
+    width: 100%;
+    height: 100%;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  }
+`;
